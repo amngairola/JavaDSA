d2ba061501f0bb4dba2ff1cb0627f9ffb5b61e2c
Aman Gaiola <amngairola@gmail.com>
2024-02-02 18:31:50 +0530

    basic recursion

---
+++ BasicRecursion/PrintNums.java
@@ -0,0 +1,31 @@
+package BasicRecursion;
+
+public class PrintNums {
+    static int num = 0;
+
+    public static void main(String[] args) {
+        print(10);
+    }
+
+    // print
+    /*
+     * static void print(int n) {
+     * if (num < n) {
+     * System.out.println(num);
+     * num++;
+     * print(n);
+     * }
+     * 
+     * }
+     */
+
+    // print n-1
+    static void print(int n) {
+        if (n > 1) {
+            System.out.println(n);
+            n--;
+            print(n);
+        }
+
+    }
+}
