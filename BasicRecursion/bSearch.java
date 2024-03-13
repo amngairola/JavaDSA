4110c935111946ce63366328aa755458417b21b3
Aman Gaiola <amngairola@gmail.com>
2024-03-13 08:45:03 +0530

    recurtion basic problem of b-search

--- BasicRecursion/bSearch.java
+++ BasicRecursion/bSearch.java
@@ -3,24 +3,30 @@ package BasicRecursion;
 public class bSearch {
     public static void main(String[] args) {
         int[] arr = { 2, 5, 8, 12, 16, 23, 38, 56, 72, 91 };
-        int target = 23;
-        int result = binarySearch(arr, target, 0, arr.length - 1, 0 + arr.length / 2);
+        int target = 0;
+        int result = binarySearch(arr, target, 0, arr.length - 1);
         if (result == -1)
-            System.out.println("Element not present in array");
+            System.out.println("Element not present in array" + result);
         else
             System.out.println("Element found at index " + result);
     }
 
-    public static int binarySearch(int[] arr, int target, int s, int e, int mid) {
+    public static int binarySearch(int[] arr, int target, int s, int e) {
 
-        mid = s + (e - s) / 2;
-        if (arr[mid] == target) {
-            return mid;
-        } else if (arr[mid] > target) {
-            binarySearch(arr, target, s, mid - 1, mid);
-        } else {
-            binarySearch(arr, target, mid + 1, e, mid);
+        if (s > e) {
+            return -1;
         }
-        return 0;
+
+        int m = s + (e - s) / 2;
+        {
+            if (arr[m] == target) {
+                return m;
+            } else if (arr[m] < target) {
+                // e = m -1;
+                return binarySearch(arr, target, m + 1, e);
+            }
+
+            return binarySearch(arr, target, s, m - 1);
+        }
     }
 }
