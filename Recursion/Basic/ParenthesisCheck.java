16916e6b3e21f2182ee6e081d18499a004e972ea
Aman Gaiola <amngairola@gmail.com>
2024-03-26 11:41:45 +0530

    init

--- Recursion/Basic/ParenthesisCheck.java
+++ Recursion/Basic/ParenthesisCheck.java
@@ -1,19 +1,26 @@
 package Recursion.Basic;
 
 //https://www.geeksforgeeks.org/problems/parenthesis-checker2744/1
+
 public class ParenthesisCheck {
     public static void main(String[] args) {
-        char expr[] = "[(])".toCharArray();
-        int n = expr.length;
-        if (check(expr, n))
+        String s = "()";
+        if (check(s, 0, s.length() - 1))
             System.out.println("Balanced");
         else
             System.out.println("Not Balanced");
     }
 
-    static boolean check(char[] exp, int n) {
+    static boolean check(String str, int s, int e) {
+        if (s > e) {
+            return true;
+        }
+        if (str.charAt(s) != str.charAt(e)) {
+            return false;
+        }
 
-        return false;
+        return check(str, s + 1, e - 1);
+
     }
 
 }
