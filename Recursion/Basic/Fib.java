3af01ae909af22225c58f328ca2aad6771c67c16
Aman Gaiola <amngairola@gmail.com>
2024-04-03 09:48:41 +0530

    dibboncaii formula updated

--- Recursion/Basic/Fib.java
+++ Recursion/Basic/Fib.java
@@ -2,14 +2,25 @@ package Recursion.Basic;
 
 public class Fib {
     public static void main(String[] args) {
-        System.out.println(print(50));
+        for (int i = 0; i < 50; i++) {
+            System.out.println((int) fibbo(i));
+
+        }
     }
 
-    static int print(int n) {
-        if (n < 2) {
-            return n;
-        }
+    // static int print(int n) {
+    // if (n < 2) {
+    // return n;
+    // }
 
-        return print(n - 1) + print(n - 2);
+    // return print(n - 1) + print(n - 2);
+    // }
+
+    static double fibbo(int n) {
+        double phi = (1 + Math.sqrt(5)) / 2;
+        double psi = (1 - Math.sqrt(5)) / 2;
+        double fib = (Math.pow(phi, n) - Math.pow(psi, n)) / Math.sqrt(5);
+
+        return fib;
     }
 }
