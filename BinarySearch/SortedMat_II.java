c4b0d7d1903814fe93be51bac3ce527a495c5cc7
Aman Gaiola <amngairola@gmail.com>
2024-08-01 23:44:09 +0530

    Median in a row-wise sorted Matrix

---
+++ BinarySearch/SortedMat_II.java
@@ -0,0 +1,25 @@
+public class SortedMat_II {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        int row = 0;
+        int col = m - 1;
+
+        while (row < n && col >= 0) {
+            if (matrix[row][col] == target)
+                return true;
+            else if (matrix[row][col] < target)
+                row++;
+            else
+                col--;
+
+        }
+        return false;
+
+    }
+}
